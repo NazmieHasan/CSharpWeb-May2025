b19b0118c8e7a4040c87f90f10ea6f1aa3730118
NazmieHasan <Nazi87@abv.bg>
2025-07-25 20:09:16 +0300

    Add category soft delete migration and delete category view

---
+++ HotelApp.Web.ViewModels/Category/DeleteCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelApp.Web.ViewModels.Category
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class DeleteCategoryViewModel
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? ImageUrl { get; set; }
+    }
+}
